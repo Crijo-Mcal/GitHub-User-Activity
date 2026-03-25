2ee0688ea7a85032542fbcf3a6d5c7857f1cb45e
Crijolise Morglia <crijolisemor25@gmail.com>
2026-03-25 22:57:12 +0000

    correction spase

--- aplication_cli/api.js
+++ aplication_cli/api.js
@@ -3,44 +3,32 @@ const endpoint = "https://api.github.com/users";
 
 export async function getData(userName) {
 
-
     try {
-
-
         await checkUserExists(userName);
-
         const events = await getUserEvents(userName)
-
 
         if (events.length === 0) {
             console.log("No public events found.");
             return;
         }
-
         console.log('\n', `ACTIVITY OF USERNAME : "${userName}"`, '\n');
-
-
         for (const event of events) {
             switch (event.type) {
-
                 case 'IssuesEvent':
                     console.log(
                         `- ${event.payload.action} an issue in ${event.repo.name}`
                     );
                     break;
-
                 case 'PublicEvent':
                     console.log(
                         `- Made the repository public: ${event.repo.name}`
                     );
                     break;
-
                 case 'CreateEvent':
                     console.log(
                         `- Created a new branch in repository: ${event.repo.name}`
                     );
                     break;
-
                 case 'PullRequestEvent':
                     console.log(
                         `- ${event.payload.action} pull request "${event.payload.pull_request.title}" in ${event.repo.name}`
@@ -72,7 +60,6 @@ async function checkUserExists(userName) {
 
     /* handle error */
     const userResponse = await fetch(`https://api.github.com/users/${userName}`)
-
 
     if (userResponse.status === 404) {
         throw new Error('user name not found')
--- aplication_cli/apss.js
+++ aplication_cli/apss.js
@@ -1,8 +1,6 @@
 import { getData } from "./api.js";
 const argv = process.argv.slice(2);
 
-
 if (argv[0] == "github-activity") {
     getData(argv[1]);
-
 }
